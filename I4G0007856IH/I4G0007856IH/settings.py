d4cf1837996dedfd7e0c80a4675282487b96ad19
dariemjnr <dariemjnr3@gmail.com>
2022-06-22 12:17:56 +0100

    commit

--- I4G0007856IH/I4G0007856IH/settings.py
+++ I4G0007856IH/I4G0007856IH/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newapp'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'new app'
 ]
 
 ROOT_URLCONF = 'naandam.urls'
